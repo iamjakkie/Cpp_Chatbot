770a446563b3b96f84f72fdb2d636fab3e4e60d8
jakkie <jakubkielbasiewicz@gmail.com>
2022-01-06 10:33:23 +0100

    Fixed graphnode pointers for make

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -70,7 +70,7 @@ ChatBot::ChatBot(ChatBot&& source){
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
     _image = source._image;
-    
+    _chatLogic->SetChatbotHandle(this);
     source._chatLogic = nullptr;
     source._rootNode = nullptr;
     source._image = NULL;
@@ -86,12 +86,12 @@ ChatBot& ChatBot::operator=(ChatBot&& source){
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
     _image = source._image;
-    
+    _chatLogic->SetChatbotHandle(this);
     source._chatLogic = nullptr;
     source._rootNode = nullptr;
     source._image = NULL;
 
-    return *this;
+    
 
 }
 
